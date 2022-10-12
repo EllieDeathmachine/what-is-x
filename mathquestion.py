d95ff39c278bd090262f86a178487b8227a61c84
UpsetSkulls <115660733+UpsetSkulls@users.noreply.github.com>
2022-10-12 12:48:12 -0700

    Update mathquestion.py

--- mathquestion.py
+++ mathquestion.py
@@ -6,7 +6,7 @@ numb = random.randrange(1,10)
 print("hello, welcome to Upset's math!")
 time.sleep(1)
 print(numb, "+ x =", x + numb)
-answer = input("what is x?")
+answer = input("what is x? ")
 if answer == x:
   print("correct")
 else:
